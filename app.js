12e97fc69b50a20088c0f42029c668a7985ae85b
gabrielx52 <gabriel.meringolo@gmail.com>
2017-08-08 14:30:07 -0700

    First question done on JS, started CSS styling.

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 'use strict';
-var yesNo = 'Please answer Yes/No or Y/N.';
 
-var varp1 = prompt(yesNo).toLowerCase();
-alert(varp1);
-console.log();
-var varp2 = prompt(yesNo).toLowerCase();
+var yesNo = '[Please answer Yes/No or Y/N.]';
 
-console.log();
-var varp3 = prompt(yesNo).toLowerCase();
+alert('Welcome to the amazing Guess-O! Using my amazing powers of deduction I will make five(5) highly probable predictions about you and you will tell me if I am correct or not. Lets begin!');
 
-console.log();
-var varp4 = prompt(yesNo).toLowerCase();
-
-console.log();
-var varp5 = prompt(yesNo).toLowerCase();
-
-console.log();
+var banKetchup = prompt('Prediction number One(1)!\nI predict that you believe putting ketchup on hotdogs is a crime slightly worse then treason!\n' + yesNo).toLowerCase();
+alert(banKetchup);
+console.log(banKetchup);
+// var varp2 = prompt(yesNo).toLowerCase();
+//
+// console.log(varp2);
+// var varp3 = prompt(yesNo).toLowerCase();
+//
+// console.log(varp3);
+// var varp4 = prompt(yesNo).toLowerCase();
+//
+// console.log(varp4);
+// var varp5 = prompt(yesNo).toLowerCase();
+//
+// console.log(varp5);
