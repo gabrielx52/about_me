894747cc177015457440b1fa43cdd3e85d7791c7
gabrielx52 <gabriel.meringolo@gmail.com>
2017-08-08 15:35:45 -0700

    Finished with questions.

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ var isKetchupACrime = prompt('Prediction number One(1)!\nI predict that you beli
 alert(isKetchupACrime);
 console.log(isKetchupACrime);
 
-var whoShotFirstHanOrGreedo = prompt('Amazing prediction number Two(2)!\nUsing my superior RAM strength I have deduced that you know in your heart of hearts that Greedo, absolutley never, EVER, shot first.\n' + yesNo).toLowerCase();
-alert(whoShotFirstHanOrGreedo);
-console.log(whoShotFirstHanOrGreedo);
+var didHanShootFirst = prompt('Amazing prediction number Two(2)!\nUsing my superior RAM strength I have deduced that you know in your heart of hearts that Greedo, absolutley never, EVER, shot first.\n' + yesNo).toLowerCase();
+alert(didHanShootFirst);
+console.log(didHanShootFirst);
 
-var lovesCheese = prompt('Possible accurate prediction number Three(3)!!!\nFrom the impressive size of your shoelaces, I have calculated that you really like cheese, like really, really like it. Almost unnaturally. Amirightoramiright?\n' + yesNo).toLowerCase();
-alert(lovesCheese);
-console.log(lovesCheese);
+var getsRoniOnThePizz = prompt('Possible accurate prediction number Three(3)!!!\nFrom the impressive size of your shoelaces, I have calculated that you always get pepperoni on your pizza. Amirightoramiright?\n' + yesNo).toLowerCase();
+alert(getsRoniOnThePizz);
+console.log(getsRoniOnThePizz);
 
-var isARock = prompt('Absurd assumption number Four(4)!\nYou are not a rock!\n' + yesNo).toLowerCase();
+var isARock = prompt('Absurd assumption number Four(4)!\nYou are a rock! Are you a rock person??\n' + yesNo).toLowerCase();
 alert(isARock);
 console.log(isARock);
 
-var varp5 = prompt('\n' + yesNo).toLowerCase();
-alert(varp5);
-console.log(varp5);
+var isRightHanded = prompt('Fifth and final prediction number Five(5)!!\nUsing all of my highly advanced powers of deduction, I have come to the conclusion that you are right handed!\n' + yesNo).toLowerCase();
+alert(isRightHanded);
+console.log(isRightHanded);
