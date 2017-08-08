091bff2767af40e81fb75b87d74f65d7e0683ee8
gabrielx52 <gabriel.meringolo@gmail.com>
2017-08-08 14:47:02 -0700

    Started nav bar in HTML, formating in JS.

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var yesNo = '[Please answer Yes/No or Y/N.]';
 
 alert('Welcome to the amazing Guess-O! Using my amazing powers of deduction I will make five(5) highly probable predictions about you and you will tell me if I am correct or not. Lets begin!');
 
-var banKetchup = prompt('Prediction number One(1)!\nI predict that you believe putting ketchup on hotdogs is a crime slightly worse then treason!\n' + yesNo).toLowerCase();
-alert(banKetchup);
-console.log(banKetchup);
+var isKetchupACrime = prompt('Prediction number One(1)!\nI predict that you believe putting ketchup on hotdogs is a crime slightly worse then treason!\n' + yesNo).toLowerCase();
+alert(isKetchupACrime);
+console.log(isKetchupACrime);
 
 var whoShotFirstHanOrGreedo = prompt('Amazing prediction number Two(2)!\nUsing my superior RAM strength I have deduced that you know in your heart of hearts that Greedo, absolutley never, EVER, shot first.\n' + yesNo).toLowerCase();
 altert(whoShotFirstHanOrGreedo);
