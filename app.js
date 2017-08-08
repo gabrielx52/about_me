06f6ffc2445a6f9a3a4dd2a94e09cb4f40d0b5de
gabrielx52 <gabriel.meringolo@gmail.com>
2017-08-08 14:02:19 -0700

    Fixed script tag in HTML, started JS skeleton.

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var varp = prompt('hello').toLowerCase();
+alert(varp);
+
+var varp = prompt().toLowerCase();
+
+var varp = prompt().toLowerCase();
+
+var varp = prompt().toLowerCase();
+
+var varp = prompt().toLowerCase();
