81cc906286a59bf11996853c28187e61bd75c143
gabrielx52 <gabriel.meringolo@gmail.com>
2017-08-08 14:39:11 -0700

    Added second question to JS.

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ alert('Welcome to the amazing Guess-O! Using my amazing powers of deduction I wi
 var banKetchup = prompt('Prediction number One(1)!\nI predict that you believe putting ketchup on hotdogs is a crime slightly worse then treason!\n' + yesNo).toLowerCase();
 alert(banKetchup);
 console.log(banKetchup);
-// var varp2 = prompt(yesNo).toLowerCase();
-//
-// console.log(varp2);
+
+var whoShotFirstHanOrGreedo = prompt('Amazing prediction number Two(2)!\nUsing my superior RAM strength I have deduced that you know in your heart of hearts that Greedo, absolutley never, EVER, shot first.\n' + yesNo).toLowerCase();
+altert(whoShotFirstHanOrGreedo);
+console.log(whoShotFirstHanOrGreedo);
+
 // var varp3 = prompt(yesNo).toLowerCase();
 //
 // console.log(varp3);
