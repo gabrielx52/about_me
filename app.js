76655c13dcb5b62cc4ccbf7c20f40dfba128fa60
gabrielx52 <gabriel.meringolo@gmail.com>
2017-08-08 14:12:22 -0700

    Added more structure to HTML and JS files.

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 'use strict';
+var yesNo = 'Please answer Yes/No or Y/N.';
 
-var varp = prompt('hello').toLowerCase();
-alert(varp);
+var varp1 = prompt(yesNo).toLowerCase();
+alert(varp1);
+console.log();
+var varp2 = prompt(yesNo).toLowerCase();
 
-var varp = prompt().toLowerCase();
+console.log();
+var varp3 = prompt(yesNo).toLowerCase();
 
-var varp = prompt().toLowerCase();
+console.log();
+var varp4 = prompt(yesNo).toLowerCase();
 
-var varp = prompt().toLowerCase();
+console.log();
+var varp5 = prompt(yesNo).toLowerCase();
 
-var varp = prompt().toLowerCase();
+console.log();
