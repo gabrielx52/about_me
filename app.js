3f4d07f83a9e0a9a1f0cb2a89aa6cec98af7af9a
gabrielx52 <gabriel.meringolo@gmail.com>
2017-08-08 17:44:14 -0700

    Added alerts to JS

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ alert('Welcome, lets see how well you know Gabriel.');
 var ketchupOnHotDog = prompt('Question One(1)\nDoes Gabe put ketchup on his hotdog?\n' + yesNo).toLowerCase();
 console.log('Q: Is ketchup a crime? A: ' + ketchupOnHotDog);
 if (ketchupOnHotDog === 'y' || ketchupOnHotDog === 'yes' || ketchupOnHotDog === 'n' || ketchupOnHotDog === 'no'){
-  alert('Accepted answer.');
+  alert('You answered: ' + ketchupOnHotDog + '\nThe correct answer is: No, he would never do that.');
 }else{
   alert('That is not an acceptable answer.');
 }
@@ -15,7 +15,7 @@ if (ketchupOnHotDog === 'y' || ketchupOnHotDog === 'yes' || ketchupOnHotDog === 
 var ownFlipFlops = prompt('Question Two(2)\nDoes Gabriel own flip flops?\n' + yesNo).toLowerCase();
 console.log('Q: Did Han shoot first? A: ' + ownFlipFlops);
 if (ownFlipFlops === 'y' || ownFlipFlops === 'yes' || ownFlipFlops === 'n' || ownFlipFlops === 'no'){
-  alert('Accepted answer.');
+  alert('You answered: ' + ownFlipFlops + '\nThe correct answer is: No, flip flops are the reason people get eaten by bears or get caught in lava flows. Can\'t run in flippys');
 }else{
   alert('That is not an acceptable answer.');
 }
@@ -23,7 +23,7 @@ if (ownFlipFlops === 'y' || ownFlipFlops === 'yes' || ownFlipFlops === 'n' || ow
 var getsRoniOnThePizz = prompt('Question Three(3)\nDoes he always order pepperoni on his pizza?\n' + yesNo).toLowerCase();
 console.log('Q: Always order pepperoni on the pizza? A: ' + getsRoniOnThePizz);
 if (getsRoniOnThePizz === 'y' || getsRoniOnThePizz === 'yes' || getsRoniOnThePizz === 'n' || getsRoniOnThePizz === 'no'){
-  alert('Accepted answer.');
+  alert('You answered: ' + getsRoniOnThePizz + '\nThe correct answer is: Yes, aways.');
 }else{
   alert('That is not an acceptable answer.');
 }
@@ -31,7 +31,7 @@ if (getsRoniOnThePizz === 'y' || getsRoniOnThePizz === 'yes' || getsRoniOnThePiz
 var isAMolePerson = prompt('Question Four(4)\nIs Gabe a moleperson?\n' + yesNo).toLowerCase();
 console.log('Q: Are you a rock? A: ' + isAMolePerson);
 if (isAMolePerson === 'y' || isAMolePerson === 'yes' || isAMolePerson === 'n' || isAMolePerson === 'no'){
-  alert('Accepted answer.');
+  alert('You answered: ' + isAMolePerson + '\nThe correct answer is: No, but he would choose that over flight or the ability to breath under water.');
 }else{
   alert('That is not an acceptable answer.');
 }
@@ -39,7 +39,7 @@ if (isAMolePerson === 'y' || isAMolePerson === 'yes' || isAMolePerson === 'n' ||
 var isRightHanded = prompt('Question Five(5)\nIs Gabriel right handed?\n' + yesNo).toLowerCase();
 console.log('Q: Are you right handed? A: ' + isRightHanded);
 if (isRightHanded === 'y' || isRightHanded === 'yes' || isRightHanded === 'n' || isRightHanded === 'no'){
-  alert('Accepted answer.');
+  alert('You answered: ' + isRightHanded + '\nThe correct answer is: Yes, yes he is.');
 }else{
   alert('That is not an acceptable answer.');
 }
